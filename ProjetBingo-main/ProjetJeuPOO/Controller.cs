d4467927fe913a3e5110504b434da2b7b5527091
Katia D <91914829+Projet1kat@users.noreply.github.com>
2022-01-13 09:33:57 -0500

    il s'appelle bingo mais c'est pendu
    comme dirait Nancy Lol...

---
+++ ProjetBingo-main/ProjetJeuPOO/Controller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ProjetJeuPOO.Bingo;
+using ProjetJeuPOO.SimiliBlackJack;
+using ProjetJeuPOO.SimiliPendu;
+
+namespace ProjetJeuPOO
+{
+    class Controller
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merci d'avoir choisi le jeu du SimiliPendu");
+        }
+
+        private void afficherMenu()
+        {
+            string choix;
+            do
+            {
+                Console.WriteLine("MENU");
+                Console.WriteLine("1 - Bingo");
+                Console.WriteLine("2 - Black Jack");
+                Console.WriteLine("3 - Pendu");
+                Console.WriteLine("4 - Quitter");
+
+                choix = Console.ReadLine();
+                selectChoice(choix);
+            } while (choix != "4");
+        }
+
+
+        private void selectChoice(string selection)
+        {
+
+            switch (selection)
+            {
+
+                case "1":
+                    jouerBingo();
+                    break;
+                case "2":
+                    jouerBlackJack();
+                    break;
+                case "3":
+                    jouerPendu();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        private void jouerBingo() 
+        {
+        }
+        private void jouerBlackJack() 
+        {
+        }
+        private void jouerPendu() 
+        {
+        }
+    }
+}
+
